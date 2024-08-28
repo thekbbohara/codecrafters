6c52db6edc77cc8f4759b5809156de772d5b8be6
kbzcraft <ytkbzcraft@gmail.com>
2024-08-28 17:09:11 +0545

    reduced code

--- js/codecrafters-grep-javascript/app/cheated.js
+++ js/codecrafters-grep-javascript/app/cheated.js
@@ -1,25 +1,8 @@
-function matchPattern(inputLine, pattern) {
-  const reg = new RegExp(pattern);
-  return reg.test(inputLine);
+const pattern = process.argv[3];
+const inputLine = require("fs").readFileSync(0, "utf-8").trim();
+if (process.argv[2] !== "-E") {
+  console.log("Expected first argument to be '-E'");
+  process.exit(1);
 }
-function main() {
-  const pattern = process.argv[3];
-  const inputLine = require("fs").readFileSync(0, "utf-8").trim();
-
-  console.log({ pattern, inputLine });
-
-  if (process.argv[2] !== "-E") {
-    console.log("Expected first argument to be '-E'");
-    process.exit(1);
-  }
-
-  // Uncomment this block to pass the first stage
-  if (matchPattern(inputLine, pattern)) {
-    process.exit(0);
-  } else {
-    console.log("exitCode: 1");
-    process.exit(1);
-  }
-}
-
-main();
+const reg = new RegExp(pattern);
+process.exit(!reg.test(inputLine));
